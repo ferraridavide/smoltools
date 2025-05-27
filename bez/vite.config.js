7dbbfcdce71138129b15120b88a73b3999feda6e
Davide Ferrari <xthehacker2000x@gmail.com>
2025-05-27 07:48:57 +0000

    🌐 Moved to subdomain

--- bez/vite.config.js
+++ bez/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/smoltools/bez/",
+  base: "/bez/",
   build: {
 	  outDir: '../dist/bez',
     emptyOutDir: true
